77a60b5e59ea63ffa0eb500f82283b57974eb3d4
Oleksandr <alexundercover191@gmail.com>
2023-11-06 13:24:51 +0200

    Add class helpers

---
+++ Assets/Scripts/TouchUtills.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class TouchUtills : MonoBehaviour
+{
+    public static Vector3 ScreenToWorld(Camera camera, Vector3 position)
+    {
+        position.z = camera.nearClipPlane;
+        return camera.ScreenToWorldPoint(position);
+    }
+}
