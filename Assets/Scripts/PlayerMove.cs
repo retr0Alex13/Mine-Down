5bbfdacbb59d6144775dd4680ad23add234bca78
Oleksandr <alexundercover191@gmail.com>
2023-11-06 13:32:14 +0200

    Add player move script

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField]
+    private SwipeDetection swipeDetection;
+    [SerializeField]
+    private float blockWidth = 1f;
+    [SerializeField]
+    private float moveSpeed = 15f;
+
+    private Rigidbody body;
+    private Vector3 targetPosition;
+    private Vector2 swipeDirection;
+
+    private void Start()
+    {
+        swipeDetection.OnSwipeDirection += MovePlayer;
+        body = GetComponent<Rigidbody>();
+    }
+
+    private void MovePlayer(Vector2 direction)
+    {
+        swipeDirection = direction;
+        StartCoroutine(MoveToPoint());
+    }
+
+    private IEnumerator MoveToPoint()
+    {
+        CheckTargetDirection();
+
+        while (Vector3.Distance(transform.position, targetPosition) >= 0f)
+        {
+            Vector3 newPosition = Vector3.Lerp(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+            body.MovePosition(newPosition);
+            yield return null;
+        }
+    }
+
+    private void CheckTargetDirection()
+    {
+        if (swipeDirection == Vector2.left)
+        {
+            targetPosition = transform.position + new Vector3(-1f, 0f, 0f) * blockWidth;
+        }
+        else if (swipeDirection == Vector2.right)
+        {
+            targetPosition = transform.position + new Vector3(1f, 0f, 0f) * blockWidth;
+        }
+    }
+}
