1a1569619e92134deb6fd8ca1f07f426a351f016
Oleksandr <alexundercover191@gmail.com>
2023-11-26 18:15:27 +0200

    Add collapse that moving down the level, increase speed with each finished level

---
+++ Assets/Scripts/CollapseController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class CollapseController : MonoBehaviour
+{
+    [SerializeField] private float spawnCollapseDelay = 2f;
+    [SerializeField] private float collapseOffsetSpawn = 10f;
+
+    private Vector3 spawnPosition;
+
+    private void OnEnable()
+    {
+        OnLevelStartTrigger.OnLevelStartTriggered += HandleCollapsePosition;
+    }
+
+    private void OnDisable()
+    {
+        OnLevelStartTrigger.OnLevelStartTriggered -= HandleCollapsePosition;
+    }
+
+    private void HandleCollapsePosition(Vector3 startLevelPosition)
+    {
+        spawnPosition = startLevelPosition;
+        StartCoroutine(EnableCollapse());
+    }
+
+    private IEnumerator EnableCollapse()
+    {
+        yield return new WaitForSeconds(spawnCollapseDelay);
+        transform.position = new Vector3(spawnPosition.x, spawnPosition.y + collapseOffsetSpawn);
+    }
+}
