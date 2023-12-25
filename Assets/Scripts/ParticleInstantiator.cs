17dd78c9bd5478979140080a71ef6ed941eb63c8
Oleksandr <alexundercover191@gmail.com>
2023-12-25 18:47:48 +0200

    Change particles instantiating angle

--- Assets/Scripts/ParticleInstantiator.cs
+++ Assets/Scripts/ParticleInstantiator.cs
@@ -14,15 +14,15 @@ public class ParticleInstantiator : MonoBehaviour
     {
         if (direction == Vector2.up || direction == Vector2.down)
         {
-            return Quaternion.Euler(-90f, 0f, 0f);
+            return Quaternion.Euler(220f, 0f, 0f);
         }
         else if (direction == Vector2.right)
         {
-            return Quaternion.Euler(-50f, 95f, 0f);
+            return Quaternion.Euler(-50f, 150f, 0f);
         }
         else if (direction == Vector2.left)
         {
-            return Quaternion.Euler(-150f, 70f, 0f);
+            return Quaternion.Euler(-50, -150f, 0f);
         }
 
         return Quaternion.identity; // Default rotation
