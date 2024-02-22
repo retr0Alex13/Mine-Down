b459be4f4e5eaf534675f52f450b25a0929bced7
Oleksandr Moskalyk <alexundercover191@gmail.com>
2024-02-22 21:57:42 +0200

    Replace parallax effect with another way

---
+++ Assets/Scripts/ParallaxBackground.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class ParallaxBackground : MonoBehaviour
+{
+    [SerializeField] private Vector2 parallaxEffectMultiplier;
+
+    private Transform cameraTransform;
+    private Vector3 lastCameraPosition;
+    private float textureUnitSizeY;
+
+    private void Awake()
+    {
+        cameraTransform = Camera.main.transform;
+        lastCameraPosition = cameraTransform.position;
+
+        Sprite sprite = GetComponent<SpriteRenderer>().sprite;
+        Texture2D texture = sprite.texture;
+        textureUnitSizeY = texture.height / sprite.pixelsPerUnit;
+    }
+
+    private void Update()
+    {
+        Vector3 deltaMovement = cameraTransform.position - lastCameraPosition;
+        transform.position += new Vector3(deltaMovement.x * parallaxEffectMultiplier.x, deltaMovement.y * parallaxEffectMultiplier.y);
+        lastCameraPosition = cameraTransform.position;
+
+        if (Mathf.Abs(cameraTransform.position.y - transform.position.y) >= textureUnitSizeY)
+        {
+            float offsetPositionY = (cameraTransform.position.y - transform.position.y) % textureUnitSizeY;
+            transform.position = new Vector3(transform.position.x, cameraTransform.position.y + offsetPositionY);
+        }
+    }
+}
